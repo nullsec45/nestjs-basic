e680a20e50604765e326af29a9424b628f557d27
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-22 22:45:45 +0700

    params, response json, redirect.

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Req, Res, Header, HttpCode, HttpRedirectResponse, Redirect } from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Controller('user')
+
+@Controller('/api/users')
 export class UserController {
+    @Get('/hello')
+    sayHello(@Query('first_name') firstName:string, @Query('last_name') lastName:string):string{
+        return `Hello ${firstName} ${lastName}`;
+    }
+
     @Post()
     posts():string{
         return 'POST';
@@ -10,5 +17,34 @@ export class UserController {
     @Get('/sample')
     get():string{
         return 'GET';
+    }
+
+    @Get('/sample-response')
+    sampleResponse(@Res() response:Response){
+        response.status(200).send('Sample Response');
+    }
+
+    @Get('/sample-response-json')
+    @Header('Content-Type','application/json')
+    @HttpCode(200)
+    sampleResponseJSON():Record<string,string>{
+        return {
+            data : 'Hello JSON'
+        }
+    }
+
+    @Get('/redirect')
+    @Redirect()
+    redirect():HttpRedirectResponse{
+        return {
+            url:'/api/users/sample-response',
+            statusCode:301
+        }
+    }
+
+
+    @Get('/:id')
+    getById(@Req() request :Request):string{
+        return `GET ${request.params.id};`
     }
 }
