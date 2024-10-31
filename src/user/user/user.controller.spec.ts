4aa0659e37fbc8782ad3893bab1e0b729136c9ef
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-31 23:54:11 +0700

    user controller test.

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,12 +9,25 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers:[UserService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response=await controller.sayHello('Fajar','Rama');
+    expect(response).toBe('Hello Fajar');
+  });
+
+  it('should can view template', async() => {
+    const response=httpMock.createResponse();
+    controller.viewHellO('Fajar', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name:'Fajar',
+      title:'Template Engine'
+    });
   });
 });
