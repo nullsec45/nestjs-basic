7556efd5f76c038d0a94a4c4949608d9f5d46633
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-31 23:55:19 +0700

    integration test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { first } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -21,4 +22,16 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('should can say hello', async() => {
+    const result=await request(app.getHttpServer())
+                      .get('/api/users/hello')
+                      .query({
+                        first_name:'Rama',
+                        last_name:'Fajar'
+                      });
+
+    expect(result.status).toBe(200);
+    expect(result.text).toBe('Hello Rama Fajar');
+  })
 });
