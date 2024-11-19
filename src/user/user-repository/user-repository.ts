b125ef3b160593d4de6b7cd48b0f4f8a4d45c4b8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-19 22:37:28 +0700

    nest logger

--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,16 +1,23 @@
 import { PrismaService } from "src/prisma/prisma/prisma.service";
 import { Connection } from "../connection/connection";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject } from "@nestjs/common";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import {Logger} from 'winston';
 
 @Injectable()
 export class UserRepository {
     connection:Connection;
     
-    constructor(private prismaService:PrismaService){
-
+    constructor(
+        private prismaService:PrismaService,
+        @Inject(WINSTON_MODULE_PROVIDER) private logger:Logger
+    ){
+        this.logger.info('User Repository');
     }
 
     save(first_name:string, last_name:string){
+        this.logger.info(`create user with firstName ${first_name} and lastName ${last_name}`);
+
        return this.prismaService.user.create({
         data:{
             first_name,
