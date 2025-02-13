2020b00559d254a6d1bafebbc0b139d1eb8a6ac8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-02-13 11:55:08 +0700

    modified : lifecycle event.

--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy{
     constructor(){
         super();
+    }
+
+    onModuleDestroy() {
+        console.info('Connect Prisma');
+        this.$connect();
+    }
+
+    onModuleInit() {
+        console.info('Connect Prisma');
+        this.$disconnect();
     }
 }
